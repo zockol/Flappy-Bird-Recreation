9d485250d9f49975b2128029e660b7622ba89dd2
zockol <pz113634@fh-muenster.de>
2022-10-15 16:10:30 +0200

    Score in Endscreen
    added score in endscreen

--- Assets/Scripts/ScoreToEndScreen.cs
+++ Assets/Scripts/ScoreToEndScreen.cs
@@ -13,6 +13,6 @@ public class ScoreToEndScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<UnityEngine.UI.Text>().text = ScoreCounter.Score.ToString();
     }
 }
