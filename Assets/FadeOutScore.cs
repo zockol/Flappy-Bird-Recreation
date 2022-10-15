565729482c2bfd126df9be25c8273ab76a70d8c4
zockol <pz113634@fh-muenster.de>
2022-10-15 15:44:41 +0200

    Score Fade In/Out
    Score fades in or out wether the player started a new game or failed

---
+++ Assets/FadeOutScore.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutScore : MonoBehaviour
+{
+    private bool FadedIn;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > 0) {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Animator>().enabled = true;
+            GetComponent<Animator>().Play("Score Fade In");
+            FadedIn = true;
+        }
+        if (Time.deltaTime <= 0 && FadedIn == true) {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Animator>().enabled = true;
+            GetComponent<Animator>().Play("Score Fade Out");
+        }
+    }
+}
