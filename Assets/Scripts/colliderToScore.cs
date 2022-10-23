b87746a3b8551ef788fe87ef7ac0c46cb91f02a1
zockol <pz113634@fh-muenster.de>
2022-10-23 19:48:24 +0200

    Edited colliderToScore script
    Added an AudioSource so it would play the PointSound GameObject as soon as the collider gets triggered

--- Assets/Scripts/colliderToScore.cs
+++ Assets/Scripts/colliderToScore.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class colliderToScore : MonoBehaviour
 {
+
+    public AudioSource Point;
+
     private void OnTriggerEnter2D(Collider2D collision) {
         ScoreCounter.Score++;
+        Point.Play();
     }
 }
